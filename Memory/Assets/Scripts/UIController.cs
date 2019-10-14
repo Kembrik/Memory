cfe400da2326deb876343d8faa730a1069f0fbd3
AL_BOT <albot84@mail.ru>
2019-10-14 18:15:47 +0300

    Внедрение нового Gui

---
+++ Memory/Assets/Scripts/UIController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+	[SerializeField] private Text scoreLabel;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		scoreLabel.text = Time.realtimeSinceStartup.ToString();
+	}
+
+	public void OnOpenSettings()
+	{
+		Debug.Log("Open Settings");
+	}
+
+	public void OnMouseOver()
+	{
+		Image sprite = GetComponent<Image>();
+		if (sprite != null)
+		{
+			// sprite.color = Color.cyan;
+			sprite.color = Color.red;
+
+		}
+		Debug.Log("OnMouseOver");
+	}
+
+	public void OnMouseExit()
+	{
+		Image sprite = GetComponent<Image>();
+		if (sprite != null)
+		{
+			sprite.color = Color.cyan;
+
+		}
+		Debug.Log("OnMouseExit");
+	}
+}
